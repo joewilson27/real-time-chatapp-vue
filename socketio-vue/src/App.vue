6d766a7487ea42a23f4650777310d1aafa08dbb6
Joe Wilson <wilsonmangunsong@gmail.com>
2022-11-06 12:52:51 +0700

    adding_some_feature_to_send_message_
    part 2

--- socketio-vue/src/App.vue
+++ socketio-vue/src/App.vue
@@ -1,13 +1,67 @@
+<template>
+<div class="App">
+    <form @submit.prevent="submitToken">
+      <input type="text" placeholder="Enter token" v-model="token" />
+      <button type="submit">Submit</button>
+    </form>
+    <div class="box">
+      <div class="messages">
+        <div v-for="user in messages" :key="user.id">
+          {{user.name}}: {{user.message}}
+        </div>
+      </div>
+      <div class="messages"></div>
+      <form class="input-div" @submit.prevent="submitMessage">
+        <input type="text" placeholder="Type in text" v-model="inputMessageText" />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  </div>
+</template>
+
 <script>
 import SocketioService from '../services/socketio.service.js';
+
+// static data only for demo purposes, in real world scenario, this would be already stored on client
+const SENDER = {
+  id: "123",
+  name: "John Doe",
+};
 
 export default {
   name: 'App',
   components: {},
+  data() {
+    return {
+      token: '',
+      inputMessageText: '',
+      messages: []
+    }
+  },
+  methods: {
+    submitToken() {
+      console.log(this.token);
+      SocketioService.setupSocketConnection(this.token);
+    },
+    submitMessage() {
+      const CHAT_ROOM = "myRandomChatRoomId";
+      const message = this.inputMessageText;
+      SocketioService.sendMessage({message, roomName: CHAT_ROOM}, cb => {
+        // callback is acknowledgement from server
+        console.log(cb);
+        this.messages.push({
+          message,
+          ...SENDER
+        });
+        // clear the input after the message is sent
+        this.inputMessageText = '';
+      });
+    }
+  },
   created() {
     // this will initialize our socket connection 
     // when the created() hook fires in the lifecycle of the App component
-    SocketioService.setupSocketConnection();
+    //SocketioService.setupSocketConnection();
   },
   beforeUnmount() {
     // This will disconnect our socket as soon as the component 
@@ -18,3 +72,27 @@ export default {
   },
 }
 </script>
+
+<style>
+.App {
+  padding: 1rem;
+}
+
+.box {
+  width: fit-content;
+  height: 400px;
+  border: solid 1px #000;
+  display: flex;
+  flex-direction: column;
+  margin-top: 1rem;
+}
+
+.messages {
+  flex-grow: 1;
+}
+
+.input-div {
+  display: flex;
+  width: 100%;
+}
+</style>
